b6e18b2720aef2c8846e80fb9fde09e799e77517
Nichole De La Cruz <nichole@hackcapital.com>
2018-12-16 08:40:36 -0800

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // std::env::args returns an iterator of the command line args.
@@ -11,4 +12,7 @@ fn main() {
 
     println!("Searching for {:?}", query);
     println!("In file {:?}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents)
 }
