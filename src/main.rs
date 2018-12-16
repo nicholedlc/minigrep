f4f75b02dc3fc4b9707d8c3380e5977397be346b
Nichole De La Cruz <nichole@hackcapital.com>
2018-12-16 09:37:22 -0800

    Refactor: Extract the argument parser into a function

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,17 @@ fn main() {
     // collect is one function you often need to annotate (Rust isn’t able to infer the kind of collection you want)
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-
+    let (query, filename) = parse_config(&args);
     println!("Searching for {:?}", query);
     println!("In file {:?}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     println!("With text:\n{}", contents)
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
