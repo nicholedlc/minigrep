f80274f2eaf79ae972014f83f94bbfd0fcb63f87
Nichole De La Cruz <nichole@hackcapital.com>
2018-12-16 10:03:54 -0800

    Refactor: Rename parse_config to new and use as  constructor for config

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,29 @@ fn main() {
     // collect is one function you often need to annotate (Rust isn’t able to infer the kind of collection you want)
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {:?}", query);
-    println!("In file {:?}", filename);
+    let config = Config::new(&args);
+    println!("Searching for {:?}", config.query);
+    println!("In file {:?}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
     println!("With text:\n{}", contents)
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // `clone()` will make a full copy of the data for the Config instance to own,
+        // which takes more time and memory than storing a reference to the string data.
+        // However, cloning the data also makes our code very straightforward because we don’t have to manage the lifetimes of the references;
+        // in this circumstance, giving up a little performance to gain simplicity is a worthwhile trade-off.
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
