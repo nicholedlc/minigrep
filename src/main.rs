393be43b5c01a7ada91ada53397558858febeba1
Nichole De La Cruz <nichole@hackcapital.com>
2018-12-25 09:48:35 -0500

    Write error messages to stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,14 @@ fn main() {
     // if the value is an `Err` value, this method will pass the inner value of `Err` (the static string) to our closure
     // in the argument `err` of the anonymous function, which is defined and passed in as arg to `unwrap_or_else`
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
-    println!("Searching for {:?}", config.query);
-    println!("In file {:?}", config.filename);
 
     // Because run returns () in the success case, we only care about detecting an error,
     // so we don’t need unwrap_or_else to return the unwrapped value because it would only be ()
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1)
     }
 }
